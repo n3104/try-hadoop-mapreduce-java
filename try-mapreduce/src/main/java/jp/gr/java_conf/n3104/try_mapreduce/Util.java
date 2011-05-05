cc2c78c994f223e94473c8a89598d62666d7ade7
nt3104@gmail.com <nt3104@gmail.com@f28976a5-435b-2e82-f5c4-96b194023fe4>
2011-05-05 07:34:03 +0000

    タイムスタンプ付きのジョブの出力結果ディレクトリのパスを返すユーティリティメソッドを追加しました。

--- try-mapreduce/src/main/java/jp/gr/java_conf/n3104/try_mapreduce/Util.java
+++ try-mapreduce/src/main/java/jp/gr/java_conf/n3104/try_mapreduce/Util.java
@@ -1,5 +1,8 @@
 package jp.gr.java_conf.n3104.try_mapreduce;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * ユーティリティです。
  * 
@@ -17,9 +20,23 @@ public class Util {
 	 * @return ジョブの出力結果ディレクトリのパス
 	 */
 	public static String getJobOutputDirPath(Class<?> clazz) {
-		return "target/test/" + clazz.getPackage().getName().replace('.', '/') + "/"
-				+ clazz.getSimpleName();
+		String packagePath = clazz.getPackage().getName().replace('.', '/');
+		return "target/test/" + packagePath + "/" + clazz.getSimpleName();
 
+	}
+
+	/**
+	 * 指定されたクラスからジョブの出力結果ディレクトリのパスを生成して返します。
+	 * <p>
+	 * パスの最後にタイムスタンプを付与するため、前回の処理結果を保持したい際に利用してください。
+	 * </p>
+	 * 
+	 * @param clazz クラス
+	 * @return タイムスタンプ付きのジョブの出力結果ディレクトリのパス
+	 */
+	public static String getJobOutputDirPathWithTimestamp(Class<?> clazz) {
+		String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+		return getJobOutputDirPath(clazz) + "_" + timestamp;
 	}
 
 }
