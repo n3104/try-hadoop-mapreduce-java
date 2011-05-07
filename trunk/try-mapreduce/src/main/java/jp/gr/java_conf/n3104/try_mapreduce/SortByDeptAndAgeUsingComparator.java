51609538d5b43c7748c80b96459f910511b56579
nt3104@gmail.com <nt3104@gmail.com@f28976a5-435b-2e82-f5c4-96b194023fe4>
2011-05-07 06:44:38 +0000

    クラス名が長いため、DepartmentをDeptに変更しました。

--- trunk/try-mapreduce/src/main/java/jp/gr/java_conf/n3104/try_mapreduce/SortByDeptAndAgeUsingComparator.java
+++ trunk/try-mapreduce/src/main/java/jp/gr/java_conf/n3104/try_mapreduce/SortByDeptAndAgeUsingComparator.java
@@ -2,11 +2,13 @@ package jp.gr.java_conf.n3104.try_mapreduce;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.examples.SecondarySort.FirstGroupingComparator;
-import org.apache.hadoop.examples.SecondarySort.IntPair;
 import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -18,7 +20,6 @@ import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Partitioner;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.util.Tool;
@@ -27,54 +28,55 @@ import org.apache.hadoop.util.ToolRunner;
 /**
  * 従業員ファイルを部門と年齢でソートします。
  * <p>
- * Mapper のキーを {@code demartmentId} と {@code employeeAge} の複合キーにして、
- * シャッフル時の Secondary Sort を利用してソートしています。
+ * Mapper のキーを {@code demartmentId} にして部門単位でまずソートし、
+ * Reducer 内で {@link Comparator} を利用して年齢でソートしています。
  * </p>
  * 
  * @author n3104
  */
-public class SortByDepartmentAndAgeUsingSecondarySort extends Configured implements Tool {
+public class SortByDeptAndAgeUsingComparator extends Configured implements Tool {
 
 	public static class SortByDepartmentAndAgeMapper extends MapReduceBase implements
-			Mapper<Object, Text, IntPair, Text> {
+			Mapper<Object, Text, IntWritable, Text> {
 
 		private EmployeeRecordParser parser = new EmployeeRecordParser();
 
 		@Override
-		public void map(Object key, Text value, OutputCollector<IntPair, Text> output,
+		public void map(Object key, Text value, OutputCollector<IntWritable, Text> output,
 				Reporter reporter) throws IOException {
 			parser.parse(value);
-			IntPair intPair = new IntPair();
-			intPair.set(parser.getDepartmentId(), parser.getEmployeeAge());
-			output.collect(intPair, value);
+			output.collect(new IntWritable(parser.getDepartmentId()), value);
 		}
 	}
 
 	public static class SortByDepartmentAndAgeReducer extends MapReduceBase implements
-			Reducer<IntPair, Text, IntWritable, Text> {
-
-		private EmployeeRecordParser parser = new EmployeeRecordParser();
+			Reducer<IntWritable, Text, IntWritable, Text> {
 
 		@Override
-		public void reduce(IntPair key, Iterator<Text> values,
+		public void reduce(IntWritable key, Iterator<Text> values,
 				OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
+			List<Text> list = new ArrayList<Text>();
 			while (values.hasNext()) {
-				Text value = values.next();
-				parser.parse(value);
-				output.collect(new IntWritable(parser.getDepartmentId()), value);
+				// Iteratorの要素は参照が再利用される実装であるため、別途Textをnewする必要がある。
+				list.add(new Text(values.next()));
 			}
-		}
-	}
+			// ageでソート
+			Collections.sort(list, new Comparator<Text>() {
 
-	public static class FirstPartitioner implements Partitioner<IntPair, Text> {
+				private EmployeeRecordParser parser1 = new EmployeeRecordParser();
+				private EmployeeRecordParser parser2 = new EmployeeRecordParser();
 
-		@Override
-		public void configure(JobConf job) {
-		}
+				@Override
+				public int compare(Text o1, Text o2) {
+					parser1.parse(o1);
+					parser2.parse(o2);
+					return Integer.valueOf(parser1.getEmployeeAge()).compareTo(parser2.getEmployeeAge());
+				}
 
-		@Override
-		public int getPartition(IntPair key, Text value, int numPartitions) {
-			return Math.abs(key.getFirst() * 127) % numPartitions;
+			});
+			for (Text value : list) {
+				output.collect(key, value);
+			}
 		}
 	}
 
@@ -82,14 +84,9 @@ public class SortByDepartmentAndAgeUsingSecondarySort extends Configured impleme
 	public int run(String[] args) throws Exception {
 		JobConf conf = new JobConf(getConf(), getClass());
 		conf.setMapperClass(SortByDepartmentAndAgeMapper.class);
-		conf.setMapOutputKeyClass(IntPair.class);
-		conf.setPartitionerClass(FirstPartitioner.class);
-		conf.setOutputValueGroupingComparator(FirstGroupingComparator.class);
-
 		conf.setReducerClass(SortByDepartmentAndAgeReducer.class);
 		conf.setOutputKeyClass(IntWritable.class);
 		conf.setOutputValueClass(Text.class);
-
 		FileInputFormat.addInputPath(conf, new Path(args[0]));
 		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 		JobClient.runJob(conf);
@@ -99,12 +96,12 @@ public class SortByDepartmentAndAgeUsingSecondarySort extends Configured impleme
 	public static void main(String[] args) throws Exception {
 		// 引数を固定で設定
 		String in = "input/Employee";
-		String out = Util.getJobOutputDirPath(SortByDepartmentAndAgeUsingSecondarySort.class);
+		String out = Util.getJobOutputDirPath(SortByDeptAndAgeUsingComparator.class);
 		args = new String[] { in, out };
 		// 出力先のディレクトリが存在するとFileAlreadyExistsExceptionとなるため事前に削除しています
 		FileUtil.fullyDelete(new File(out));
 
-		int res = ToolRunner.run(new SortByDepartmentAndAgeUsingSecondarySort(), args);
+		int res = ToolRunner.run(new SortByDeptAndAgeUsingComparator(), args);
 		System.exit(res);
 	}
 
