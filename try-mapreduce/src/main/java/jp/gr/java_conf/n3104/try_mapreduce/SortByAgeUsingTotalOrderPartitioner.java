fb75c09863089178489ec205aa1b665c902ef272
nt3104@gmail.com <nt3104@gmail.com@f28976a5-435b-2e82-f5c4-96b194023fe4>
2011-09-19 15:03:23 +0000

    事前にSequenceFileを作成するように修正しました。 Update issue 1

--- try-mapreduce/src/main/java/jp/gr/java_conf/n3104/try_mapreduce/SortByAgeUsingTotalOrderPartitioner.java
+++ try-mapreduce/src/main/java/jp/gr/java_conf/n3104/try_mapreduce/SortByAgeUsingTotalOrderPartitioner.java
@@ -3,7 +3,6 @@ package jp.gr.java_conf.n3104.try_mapreduce;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
-import java.util.Iterator;
 
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.filecache.DistributedCache;
@@ -11,8 +10,6 @@ import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparable;
-import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
@@ -20,8 +17,9 @@ import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.SequenceFileInputFormat;
+import org.apache.hadoop.mapred.SequenceFileOutputFormat;
 import org.apache.hadoop.mapred.lib.HashPartitioner;
 import org.apache.hadoop.mapred.lib.InputSampler;
 import org.apache.hadoop.mapred.lib.TotalOrderPartitioner;
@@ -41,13 +39,19 @@ import org.apache.hadoop.util.ToolRunner;
  * Partitioner とは Mapper の出力をどの Reducer に振り分けるか制御するクラスを指します。
  * 部分のソートのサンプルである {@link SortByAgeUsingHashPartitioner} の場合は
  * デフォルトの {@link HashPartitioner} を利用しています。
- * この Partitioner は key のhash値に基づいて Reducer を決定する実装となっており、
+ * この Partitioner は key の hash 値に基づいて Reducer を決定する実装となっており、
  * 全体ソートには利用できません。
  * </p>
  * <p> {@link TotalOrderPartitioner} を利用する際は、どの Reducer にどの key を割り当てるかの設定を
  * 予め指定しておく必要があります。なお、実データを分析しなければ key の偏りは判別できないため、
  * Hadoop では予めいくつかの {@link InputSampler} が用意されています。
  * この {@link InputSampler} を利用して実データのサンプリングを行います。
+ * </p>
+ * <p> {@link InputSampler} は入力ファイルの key をサンプリングしてパーティション情報を作成します。
+ * そのため、 {@link InputSampler} を利用するジョブの入力ファイルの key は年齢である必要があります。
+ * 従業員ファイルをそのまま入力ファイルとした場合、 key にはファイルのバイトオフセットが設定されるため、
+ * 年齢によるソート用のパーティション情報を作成することが出来ません。
+ * そのため、事前に従業員ファイルを元に key を年齢とする SequenceFile を作成するジョブを実行しています。
  * </p>
  * <p> {@link InputSampler} 利用して作成したパーティション情報は Hadoop クラスタ上の
  * 全 Mapper に転送される必要があります。その転送処理に利用するのが {@link DistributedCache} です。 {@link DistributedCache}
@@ -71,64 +75,45 @@ public class SortByAgeUsingTotalOrderPartitioner extends Configured implements T
 		}
 	}
 
-	public static class SortByAgeReducer extends MapReduceBase implements
-			Reducer<IntWritable, Text, IntWritable, Text> {
-
-		@Override
-		public void reduce(IntWritable key, Iterator<Text> values,
-				OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
-			while (values.hasNext()) {
-				output.collect(key, values.next());
-			}
-		}
-	}
-
-	public static class DescendingKeyComparator extends WritableComparator {
-
-		protected DescendingKeyComparator() {
-			super(IntWritable.class, true);
-		}
-
-		@SuppressWarnings({ "rawtypes", "unchecked" })
-		@Override
-		public int compare(WritableComparable a, WritableComparable b) {
-			return a.compareTo(b) * -1;
-		}
-	}
-
-	public static class DescendingKeyRawComparator extends IntWritable.Comparator {
-		@Override
-		public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
-			return super.compare(b1, s1, l1, b2, s2, l2) * -1;
-		}
-	}
-
 	@Override
 	public int run(String[] args) throws Exception {
-		JobConf conf = new JobConf(getConf(), getClass());
-		conf.setMapperClass(SortByAgeMapper.class);
-		// conf.setOutputKeyComparatorClass(DescendingKeyComparator.class);
-		conf.setReducerClass(SortByAgeReducer.class);
-		conf.setOutputKeyClass(IntWritable.class);
-		conf.setOutputValueClass(Text.class);
-		FileInputFormat.addInputPath(conf, new Path(args[0]));
-		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-		// TotalOrderPartitionerを利用して全体ソートを行います。
-		conf.setPartitionerClass(TotalOrderPartitioner.class);
-		// InputSamplerを利用してパーティション情報を生成します。
-		InputSampler.Sampler<IntWritable, Text> sampler = new InputSampler.RandomSampler<IntWritable, Text>(
-				0.1, 10000, 10);
-		Path input = FileInputFormat.getInputPaths(conf)[0];
-		input = input.makeQualified(input.getFileSystem(conf));
-		Path partitionFile = new Path(input, "_partitions");
-		TotalOrderPartitioner.setPartitionFile(conf, partitionFile);
-		InputSampler.writePartitionFile(conf, sampler);
-		// DistributedCacheを利用してパーティション情報をクラスタ全体に転送します。
-		URI partitionUri = new URI(partitionFile.toString() + "#_partitions");
-		DistributedCache.addCacheFile(partitionUri, conf);
-		DistributedCache.createSymlink(conf);
-
-		JobClient.runJob(conf);
+		Path keyValuedFileDir = new Path(args[1] + "/KeyValue");
+		Path outputDir = new Path(args[1] + "/Result");
+		{
+			// 従業員ファイルから key=年齢, value=1行 のSequenceFileを作成
+			JobConf conf = new JobConf(getConf(), getClass());
+			conf.setMapperClass(SortByAgeMapper.class);
+			conf.setOutputKeyClass(IntWritable.class);
+			conf.setOutputValueClass(Text.class);
+			conf.setOutputFormat(SequenceFileOutputFormat.class);
+			FileInputFormat.addInputPath(conf, new Path(args[0]));
+			FileOutputFormat.setOutputPath(conf, keyValuedFileDir);
+			JobClient.runJob(conf);
+		}
+		{
+			// TotalOrderPartitionerを利用してソート
+			JobConf conf = new JobConf(getConf(), getClass());
+			conf.setOutputKeyClass(IntWritable.class);
+			conf.setOutputValueClass(Text.class);
+			conf.setInputFormat(SequenceFileInputFormat.class);
+			FileInputFormat.addInputPath(conf, keyValuedFileDir);
+			FileOutputFormat.setOutputPath(conf, outputDir);
+			// TotalOrderPartitionerを利用して全体ソートを行います。
+			conf.setPartitionerClass(TotalOrderPartitioner.class);
+			// InputSamplerを利用してパーティション情報を生成します。
+			InputSampler.Sampler<IntWritable, Text> sampler = new InputSampler.RandomSampler<IntWritable, Text>(
+					0.1, 10000, 10);
+			Path input = FileInputFormat.getInputPaths(conf)[0];
+			input = input.makeQualified(input.getFileSystem(conf));
+			Path partitionFile = new Path(input, "_partitions");
+			TotalOrderPartitioner.setPartitionFile(conf, partitionFile);
+			InputSampler.writePartitionFile(conf, sampler);
+			// DistributedCacheを利用してパーティション情報をクラスタ全体に転送します。
+			URI partitionUri = new URI(partitionFile.toString() + "#_partitions");
+			DistributedCache.addCacheFile(partitionUri, conf);
+			DistributedCache.createSymlink(conf);
+			JobClient.runJob(conf);
+		}
 		return 0;
 	}
 
